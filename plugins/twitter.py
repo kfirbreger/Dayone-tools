ae803d8fd7133dd8acbecfefd02ef522354f902a
Kfir Breger <info@kfirbreger.com>
2012-11-01 22:32:08 +0100

    Twitter should now work properly
    Will have to check tomorrow

--- plugins/twitter.py
+++ plugins/twitter.py
@@ -1,5 +1,5 @@
 # Core
-from datetime import datetime
+from datetime import datetime, date, timedelta, time
 # Libs
 import requests
 from requests.auth import OAuth1
@@ -14,8 +14,8 @@ class DTTwitter(dtools.Plugin):
         super(DTTwitter, self).__init__()
         # Urls used in the requests to twitter
         self.url = {
-            'favs': 'http://api.twitter.com/1/favorites.json',
-            'timeline': 'http://api.twitter.com/1/statuses/user_timeline.json'
+            'favs': 'http://api.twitter.com/1.1/favorites/list.json',
+            'timeline': 'http://api.twitter.com/1.1/statuses/user_timeline.json'
         }
         # General params to send with the request
         self.uparams = {
@@ -38,6 +38,9 @@ class DTTwitter(dtools.Plugin):
             'import_images': False,
             'favorites': True,
             'retweet': True,
+            # Replaces the twitter shortend URL with the original
+            # Note that this can make the acual text longer then 140 chars.
+            'replace_short_url': True,
             'tags': '',
             # As of 1.1 twitter requires OAuth for everything
             # Per screen name there is an access token and secret
@@ -52,37 +55,53 @@ class DTTwitter(dtools.Plugin):
         if self.config is None:
             puts(colored.blue('Config file made, please fill in the required details'))
             return
-        now = datetime.now().strftime('%Y-%m-%d %H:%M')
-        last = self.config['last_run']
-        if last.isoformat() == "1970-01-01T00:00:00":
-            last = 'start'
-        # Posts contains the posts to make. The first element is the general twit post
-        # The rest will be image including individual images
-        posts = ["##Tweets from " + last + " till " + now + "\n\n"]
+        # Geting yesterday's date
+        twitts = False
+        yest = date.today() - timedelta(days=1)
+        self.entries = [{'text': '', 'datetime': datetime.combine(yest, time.max)}]
         # Creating the import
         # for screen_name in self.config['screen_names']:
         for i in range(len(self.config['screen_names'])):
             screen_name = self.config['screen_names'][i]
+            print "Checking for " + screen_name
             oauth = OAuth1(self.config['client_key'], self.config['client_key_secret'],
                             self.config['access_token'][i], self.config['access_token_secret'][i],
                             signature_type='auth_header')
             # Adding current screen name
             self.uparams['screen_name'] = screen_name
             # Sending the request
-            r = requests.get(self.timeline, params=self.uparams, auth=oauth)
-            print r.json
+            r = requests.get(self.url['timeline'], params=self.uparams, auth=oauth)
+            #print r.json
             for item in r.json:
+                # Is it from yesterday?
+                dt = datetime.strptime(item['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+                post_date = dt.date()
+                if yest != post_date:
+                    continue
                 # Checking if there is an image
                 if self.__hasImage(item):
-                    posts.append(self.__createPost(item))
+                    self.entries.append(self.__createPost(item))
                 else:
-                    posts[0] += self.__createPostItem(item)
+                    twitts = True
+                    self.entries[0]['text'] = self.__createPostItem(item) + self.entries[0]['text']
             # Create a favourites post
-            if self.uparams['favorites']:
-                r = requests.get(self.favs, params=self.uparams)
+            if self.config['favorites']:
+                r = requests.get(self.url['favs'], params=self.uparams, auth=oauth)
+                fav_text = u''
                 for item in r.json:
-                    posts[0] += self.__createPostItem(item)  # Favs are not allowed their own entry
-        self.writeToJournal()
+                    dt = datetime.strptime(item['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+                    post_date = dt.date()
+                    if yest != post_date:
+                        continue
+                    twitts = True
+                    fav_text = self.__createPostItem(item) + fav_text  # Favs are not allowed their own entry
+                fav_text = u"### Favories\n\n" + fav_text
+            else:
+                fav_text = u''
+        # Adding title
+        self.entries[0]['text'] = "##Tweets for " + yest.strftime('%d-%m-%Y') + "\n" + self.entries[0]['text'] + fav_text
+        if twitts:
+            self.writeToJournal()
 
     def __createPostItem(self, item):
         """
@@ -92,8 +111,16 @@ class DTTwitter(dtools.Plugin):
         # @TODO Check for media and if needed add to own_entries
         p = item['created_at'].find(':')
         post_time = item['created_at'][p - 2: p + 3]
-        item_url = "https://twitter.com/" + item['user']['screen_name'] + " /status/" + item['id_str']
-        return "* [%s](%s) %s" % (post_time, item_url, item['text'])
+        item_url = u"https://twitter.com/" + unicode(item['user']['screen_name']) + u"/status/" + unicode(item['id'])
+        # Making links actually link
+        txt = item['text']
+        for link in item['entities']['urls']:
+            new_url = link['url']
+            if self.config['replace_short_url']:
+                new_url = link['expanded_url']
+            txt = txt.replace(link['url'], u'[' + new_url + u'](' + new_url + u')')
+
+        return "* [%s](%s) %s" % (post_time, item_url, txt)
 
     def __hasImage(self, item):
         """
